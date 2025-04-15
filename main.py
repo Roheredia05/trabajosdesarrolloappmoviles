9eba01571eaffd72e4917c103e655070f4ad0a73
Roheredia05 <132922121+Roheredia05@users.noreply.github.com>
2025-04-15 11:54:46 -0300

    prueba

---
+++ main.py
@@ -0,0 +1,48 @@
+#Importaciones 
+from kivymd.uix.screen import Screen
+from kivymd.app import MDApp
+from kivymd.uix.label import MDLabel
+from kivymd.uix.button import MDRaisedButton
+from kivymd.uix.boxlayout import MDBoxLayout
+from kivymd.uix.toolbar import MDTopAppBar
+
+class ContadorApp(MDApp):
+    def build(self):
+        self.contador = 0
+
+        layout = MDBoxLayout(orientation="vertical", spacing=20, padding=20)
+
+        # Toolbar
+        toolbar = MDTopAppBar(title="Contador KivyMD")
+        layout.add_widget(toolbar)
+
+        # Etiqueta del número centrado
+        self.label = MDLabel(
+            int(self.contador),    
+            halign="center",
+            font_style="H3"
+        )
+        layout.add_widget(self.label)
+
+        # Botones de + y -
+        btn_layout = MDBoxLayout(spacing=20)
+        btn_mas = MDRaisedButton(text="+", on_release=self.incrementar)
+        btn_menos = MDRaisedButton(text="-", on_release=self.reducir)
+        btn_layout.add_widget(btn_menos)
+        btn_layout.add_widget(btn_mas)
+
+        layout.add_widget(btn_layout)
+
+        screen = Screen()
+        screen.add_widget(layout)
+        return screen
+
+    def incrementar(self, instance):
+        self.contador += 1
+        self.label.text = str(self.contador)
+
+    def reducir(self, instance):
+        self.contador -= 1
+        self.label.text = str(self.contador)
+
+ContadorApp().run()
